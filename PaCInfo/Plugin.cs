7e5fd8ca219a73005955d90b10068459f5cad784
funlennysub <26184007+funlennysub@users.noreply.github.com>
2022-08-10 20:26:06 +0300

    initial commit

---
+++ PaCInfo/Plugin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Mirror;
+
+namespace PaCInfo;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+	public static ConfigEntry<WeightDisplayType> WeightDisplayType;
+	public static ConfigEntry<PriceDisplayType> PriceDisplayType;
+	public static ConfigEntry<InfoDisplayType> InfoDisplayType;
+
+	private static Harmony _harmony;
+
+	private void Awake()
+	{
+		WeightDisplayType = Config.Bind("General", "WeightDisplayType", PaCInfo.WeightDisplayType.Exact, "Weight display type");
+		PriceDisplayType = Config.Bind("General", "PriceDisplayType", PaCInfo.PriceDisplayType.Exact, "Price display type");
+		InfoDisplayType = Config.Bind("General", "InfoDisplayType", PaCInfo.InfoDisplayType.Both, "Info display type");
+
+		_harmony = Harmony.CreateAndPatchAll(typeof(PickupPatch));
+		Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+	}
+
+	private void OnDestroy()
+	{
+		_harmony.UnpatchSelf();
+	}
+}
+
+[HarmonyPatch]
+class PickupPatch
+{
+	private static double GetEstPrice(SellByWeight item, double weight)
+	{
+		var kg = weight * item.rewardPerKG;
+		var num1 = (int)kg;
+		var num2 = (int)(kg / 10.0 * LicenceManager.manage.allLicences[8].getCurrentLevel());
+		return num1 + num2;
+	}
+
+	private static double GetPrice(SellByWeight item, PriceDisplayType type) =>
+		type switch
+		{
+			PriceDisplayType.Estimate => GetEstPrice(item, GetWeight(item, WeightDisplayType.Estimate)),
+			PriceDisplayType.Exact => item.getPrice(),
+			_ => 0
+		};
+
+	private static double GetWeight(SellByWeight item, WeightDisplayType type) =>
+		type switch
+		{
+			WeightDisplayType.Estimate => Math.Round(item.myWeight - UnityEngine.Random.Range(-0.3f, 0.3f), 2, MidpointRounding.AwayFromZero),
+			WeightDisplayType.Exact => Math.Round(item.myWeight, 2, MidpointRounding.AwayFromZero),
+			_ => 0
+		};
+
+	private static string ReturnMessage(SellByWeight item, WeightDisplayType wType, PriceDisplayType pType, InfoDisplayType iType)
+	{
+		var weight = GetWeight(item, wType);
+		var price = GetPrice(item, pType);
+		return iType switch
+		{
+			InfoDisplayType.Weight => $"{item.itemName}: {EstSign(wType)}{weight}kg",
+			InfoDisplayType.Price => $"{item.itemName}: {EstSign(pType)}<sprite=11>{price:N0}",
+			InfoDisplayType.Both =>
+				$"{item.itemName}: {EstSign(wType)}{weight}kg, {EstSign(pType)}<sprite=11>{price:N0}",
+			_ => "shrug"
+		};
+	}
+
+	private static string EstSign(WeightDisplayType wType) => wType == WeightDisplayType.Estimate ? "~" : "";
+	private static string EstSign(PriceDisplayType pType) => pType == PriceDisplayType.Estimate ? "~" : "";
+
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(CharPickUp), nameof(CharPickUp.CmdPickUpObject))]
+	static void PickUpPatch(CharPickUp __instance, uint pickUpObject)
+	{
+		var spawned = NetworkIdentity.spawned[pickUpObject];
+		if (spawned is null) return;
+
+		var carriable = spawned.gameObject;
+
+		var sellByWeight = carriable.GetComponent<SellByWeight>();
+		if (sellByWeight is null) return;
+
+		NotificationManager.manage.createChatNotification(
+			ReturnMessage(sellByWeight, Plugin.WeightDisplayType.Value, Plugin.PriceDisplayType.Value, Plugin.InfoDisplayType.Value)
+		);
+	}
+}
+
+public enum WeightDisplayType
+{
+	Estimate,
+	Exact
+}
+
+public enum PriceDisplayType
+{
+	Estimate,
+	Exact
+}
+
+public enum InfoDisplayType
+{
+	Weight,
+	Price,
+	Both
+}
---
+++ PaCInfo/README.md
@@ -0,0 +1,19 @@
+﻿# PaCInfo
+
+### Description
+
+Dinkum mod that can show you the weight and price of a carriable(Ruby, Amber Chunk, etc.)
+
+### Installation
+
+- Install [BepInEx](https://builds.bepinex.dev/projects/bepinex_be/572/BepInEx_UnityMono_x64_e473f08_6.0.0-be.576.zip) by following
+  these [steps](https://docs.bepinex.dev/master/articles/user_guide/installation/unity_mono.html?tabs=tabid-win).
+
+### Configuration
+Mod comes with several options you can use:
+- `WeightDisplayType`
+  - Acceptable values: Estimate, Exact
+- `PriceDisplayType`
+  - Acceptable values: Estimate, Exact
+- `InfoDisplayType`
+  - Acceptable values: Weight, Price, Both
